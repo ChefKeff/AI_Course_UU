6ac28e5945bb1ebd35c606aaf3b36a9a0d9d38cc
ChefKeff <73639795+ChefKeff@users.noreply.github.com>
2021-09-15 11:47:59 +0200

    Create ass2_script1.3.R

---
+++ AI/ass2/ass2_script1.3.R
@@ -0,0 +1,9 @@
+library("WheresCroc")
+
+makeMoves <- function(moveInfo, readings, positions, edges, probs) {
+  print(moveInfo)
+  print(readings)
+  print(positions)
+  print(edges)
+  print(probs)
+}
